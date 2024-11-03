f59ef05d71592cedb61ec819c495669d56994a94
Josh.5 <jsunnex@gmail.com>
2024-11-04 12:39:36 +1300

    Add a new DEPLOYMENT_ID method of forcing the manager to stop the stack before starting

--- docker/overlay/init.d/start/70-start-sentry-services.sh
+++ docker/overlay/init.d/start/70-start-sentry-services.sh
@@ -5,9 +5,18 @@
 # File Created: Monday, 21st October 2024 11:40:21 am
 # Author: Josh5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Monday, 21st October 2024 11:11:51 pm
+# Last Modified: Monday, 4th November 2024 12:39:04 pm
 # Modified By: Josh5 (jsunnex@gmail.com)
 ###
+
+echo "--- Checking if Deployment ID of Sentry services has changed ---"
+if [ "$(cat ${SENTRY_DATA_PATH:?}/self_hosted/.z-deployment-id.txt 2>/dev/null)" != "${DEPLOYMENT_ID:-}" ]; then
+    echo "  - Deployment ID '${DEPLOYMENT_ID:-}' has not changed since last run. Previous ID was '$(cat ${SENTRY_DATA_PATH:?}/self_hosted/.z-deployment-id.txt 2>/dev/null)'. Stopping Sentry stack."
+    ${docker_compose_cmd:?} down --remove-orphans
+else
+    echo "  - Deployment ID '${DEPLOYMENT_ID:-}' has not changed."
+fi
+echo "${DEPLOYMENT_ID:-}" >"${SENTRY_DATA_PATH:?}/self_hosted/.z-deployment-id.txt"
 
 echo "--- Starting Sentry services ---"
 if [ "${ALWAYS_FORCE_RECREATE:-}" = "true" ]; then
