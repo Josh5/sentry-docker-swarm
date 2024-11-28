423a3ecdc4f44ec4e237d694e9e9bb82b02a77e0
Josh.5 <jsunnex@gmail.com>
2024-11-29 03:16:22 +1300

    Update redis memory restrictions to support changes from 24.11.1

--- docker/overlay/init.d/start/71-modify-running-sentry-services.sh
+++ docker/overlay/init.d/start/71-modify-running-sentry-services.sh
@@ -5,7 +5,7 @@
 # File Created: Monday, 21st October 2024 11:42:08 am
 # Author: Josh5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Friday, 29th November 2024 1:15:12 am
+# Last Modified: Friday, 29th November 2024 3:03:57 am
 # Modified By: Josh5 (jsunnex@gmail.com)
 ###
 
@@ -18,10 +18,14 @@ echo
 
 echo "--- Configure Redis memory usage ---"
 if [ "${REDIS_MEMLIMIT:-}" != "0" ]; then
-    echo "  - Configure Redis maxmemory-policy to volatile-ttl"
-    ${docker_compose_cmd:?} exec redis redis-cli CONFIG SET maxmemory-policy volatile-ttl
+    echo "  - Configure Redis maxmemory-policy to volatile-lru"
+    ${docker_compose_cmd:?} exec redis redis-cli CONFIG SET maxmemory-policy volatile-lru
     echo "  - Configure Redis maxmemory to ${REDIS_MEMLIMIT}"
-    ${docker_compose_cmd:?} exec redis redis-cli CONFIG SET maxmemory ${REDIS_MEMLIMIT}
+    ${docker_compose_cmd:?} exec redis redis-cli CONFIG SET maxmemory ${REDIS_MEMLIMIT:?}
+    if [ -f "${SENTRY_DATA_PATH:?}"/self_hosted/redis.conf ]; then
+        # From v24.11.1, there is a redis.conf file to persist this configuration
+        sed -i "s|^maxmemory.*|maxmemory ${REDIS_MEMLIMIT:?}|" "${SENTRY_DATA_PATH:?}"/self_hosted/redis.conf
+    fi
 else
     echo "  - Nothing to configure for Redis service. No value set in 'REDIS_MEMLIMIT' env variable."
 fi
