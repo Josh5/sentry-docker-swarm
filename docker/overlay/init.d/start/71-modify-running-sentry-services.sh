a99967ddf8ad38368a798ae860a0647696116128
Josh.5 <jsunnex@gmail.com>
2025-06-03 13:22:05 +1200

    Do not modify the default redis config
    Just override it with redis-cli CONFIG SET

--- docker/overlay/init.d/start/71-modify-running-sentry-services.sh
+++ docker/overlay/init.d/start/71-modify-running-sentry-services.sh
@@ -5,8 +5,8 @@
 # File Created: Monday, 21st October 2024 11:42:08 am
 # Author: Josh5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Friday, 29th November 2024 3:03:57 am
-# Modified By: Josh5 (jsunnex@gmail.com)
+# Last Modified: Tuesday, 3rd June 2025 1:21:02 pm
+# Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 
 echo "--- Configure DIND Networking ---"
@@ -22,10 +22,6 @@ if [ "${REDIS_MEMLIMIT:-}" != "0" ]; then
     ${docker_compose_cmd:?} exec redis redis-cli CONFIG SET maxmemory-policy volatile-lru
     echo "  - Configure Redis maxmemory to ${REDIS_MEMLIMIT}"
     ${docker_compose_cmd:?} exec redis redis-cli CONFIG SET maxmemory ${REDIS_MEMLIMIT:?}
-    if [ -f "${SENTRY_DATA_PATH:?}"/self_hosted/redis.conf ]; then
-        # From v24.11.1, there is a redis.conf file to persist this configuration
-        sed -i "s|^maxmemory.*|maxmemory ${REDIS_MEMLIMIT:?}|" "${SENTRY_DATA_PATH:?}"/self_hosted/redis.conf
-    fi
 else
     echo "  - Nothing to configure for Redis service. No value set in 'REDIS_MEMLIMIT' env variable."
 fi
